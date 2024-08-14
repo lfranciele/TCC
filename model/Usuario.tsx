6865686ce31e8bb181f6879270db49e16fe76dc1
lfranciele <lfranciele960@gmail.com>
2024-08-14 10:20:48 -0300

    Inicial

---
+++ model/Usuario.tsx
@@ -0,0 +1,21 @@
+class Usuario {
+    constructor(id, nome, senha, cargo) {
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+        this.cargo = cargo; 
+    }
+
+    autenticar(senha) {
+        return this.senha === senha;
+    }
+
+    atualizarSenha(novaSenha) {
+        this.senha = novaSenha;
+    }
+}
+
+const usuario = new Usuario(1, "Admin", "password", "gerente");
+if (usuario.autenticar("password")) {
+    console.log("Autenticação bem-sucedida");
+}
